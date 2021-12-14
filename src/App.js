d601df66804b7615696288fd095f5f4b2105a6b9
maxiPool <maxime.berard@gmail.com>
2021-12-13 19:43:32 -0500

    StarMatch app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Component } from 'react';
 import GitHubCardsApp from './githubcardsapp/GitHubCardsApp'
+import StarMatch from './starmatchapp/starmatchapp'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <StarMatch></StarMatch>
         <GitHubCardsApp title={'GitHubCardsApp'} />
       </div>
     );
