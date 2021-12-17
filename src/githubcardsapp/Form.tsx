671372cc99d90672cb57b298d12782df31e03a5e
maxiPool <maxime.berard@gmail.com>
2021-12-16 19:02:49 -0500

    rxJS

--- src/githubcardsapp/Form.tsx
+++ src/githubcardsapp/Form.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import { ajax } from 'rxjs/ajax'
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
 import { styles } from './style/GitHubCardsApp';
 
 class Form extends Component<{ onSubmit: Function }> {
@@ -8,9 +11,23 @@ class Form extends Component<{ onSubmit: Function }> {
   };
   handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    const resp = await axios.get(`https://api.github.com/users/${this.state.username}`);
+    // Below: axios
+    // const resp = await axios.get(`https://api.github.com/users/${this.state.username}`);
     // 'onSubmit' is the function reference passed by the parent to update the parent's data
-    this.props.onSubmit(resp.data);
+    // this.props.onSubmit(resp.data);
+    
+    // Below: rxJS
+    const url: string = `https://api.github.com/users/${this.state.username}`;
+    
+    let something3 = ajax.get(url);
+    something3.subscribe(
+      r => {
+        console.log('server response', r);
+        console.log('response content', r.response);
+        this.props.onSubmit(r.response);
+      }
+    );
+    
     // reset Form's input field
     this.setState({username: ''});  
   };
